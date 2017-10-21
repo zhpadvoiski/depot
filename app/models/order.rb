d6fc200af66e73e3f9138d1f0f885da2c221b14d
Yauheni Padvoiski <zh.padvoiski@gmail.com>
2017-10-20 20:12:12 -0500

    check out

---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
+  enum pay_type: {
+      "check" => 0,
+      "credit cart" => 1,
+      "phurchase order" => 2
+  }
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
+end
